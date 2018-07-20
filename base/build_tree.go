1ec2de7774d5ace109db33555012de89ada85046
wangzhang <wangzhang2016@gmail.com>
2018-07-20 22:32:58 +0800

    add

---
+++ base/build_tree.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	tree := createTree(arr, 0)
+	ret := printTree(tree)
+	fmt.Println(ret)
+}
+
+func printTree(node *TreeNode) (ret string) {
+	if node == nil {
+		return "#!"
+	}
+	ret += fmt.Sprintf("%d!", node.Val)
+	ret += printTree(node.Left)
+	ret += printTree(node.Right)
+	return ret
+}
+
+func createTree(arr []int, index int) *TreeNode {
+	if index >= len(arr) {
+		return nil
+	}
+	tree := &TreeNode{arr[index], nil, nil}
+	tree.Left = createTree(arr, 2*index+1)
+	tree.Right = createTree(arr, 2*index+2)
+	return tree
+}
--- leetcode4_MedianOfTwoSortedArrays.go
+++ leetcode4_MedianOfTwoSortedArrays.go
@@ -6,42 +6,46 @@ import (
 )
 
 func main() {
-	sli1 := []int{1, 3, 5, 7, 9, 11}
-	sli2 := []int{2, 4, 6, 8, 10}
+	sli1 := []int{1}
+	sli2 := []int{1}
 
 	fmt.Println("中位数为：%v\n", findMedianSortedArrays(sli1, sli2))
 }
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	total := (len(nums1) + len(nums2))
+	total := len(nums1) + len(nums2)
 
 	if total%2 == 1 {
-		return findKth(nums1, nums2, total/2+1)
+		return float64(findKth(nums1, nums2, total/2+1))
 	} else {
-		return (findKth(nums1, nums2, total/2) + findKth(nums1, nums2, total/2+1)) / 2
+		return float64(findKth(nums1, nums2, total/2)+findKth(nums1, nums2, total/2+1)) / 2
 	}
-
 }
 
-func findKth(nums1 []int, nums2 []int, k int) float64 {
+func findKth(nums1, nums2 []int, k int) int {
 	if len(nums1) > len(nums2) {
 		return findKth(nums2, nums1, k)
 	}
-	if len(nums1) == 1 {
-		return float64(nums2[k-1])
+
+	if len(nums1) == 0 {
+		return nums2[k-1]
+	}
+	if k == 1 {
+		return int(math.Min(float64(nums1[0]), float64(nums2[0])))
 	}
 
-	if k == 1 {
-		return math.Min(float64(nums1[0]), float64(nums2[0]))
+	if len(nums1) == 1 {
+		return int(math.Min(float64(nums2[k-1]), float64(nums1[0])))
 	}
 
 	pa := int(math.Min(float64(k/2), float64(len(nums1))))
 	pb := k - pa
+
 	if nums1[pa-1] < nums2[pb-1] {
 		return findKth(nums1[pa:], nums2, k-pa)
 	} else if nums1[pa-1] > nums2[pb-1] {
 		return findKth(nums1, nums2[pb:], k-pb)
 	} else {
-		return float64(nums1[pa-1])
+		return nums1[pa-1]
 	}
 }
